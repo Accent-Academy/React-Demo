bee86500410fd3309fe6d655b82c32080a727165
Parvin-Gasimli <parvinzga@code.edu.az>
2023-01-23 21:15:50 +0400

    add file

---
+++ React_lesson10/apps/src/Mock.js
@@ -0,0 +1,11 @@
+import { v4 as uuidv4 } from "uuid";
+const unique_id = uuidv4();
+const Users = [
+  { id: unique_id, name: "Azer", Surname: "Memmedov" },
+  { id: unique_id, name: "Afet", Surname: "Huseynzade" },
+  { id: unique_id, name: "Gunel", Surname: "Haciyeva" },
+  { id: unique_id, name: "Ilkin", Surname: "Sadiqov" },
+  { id: unique_id, name: "Hemid", Surname: "Yusifli" },
+  { id: unique_id, name: "Mustafa", Surname: "Aslanov" },
+];
+export default Users;
